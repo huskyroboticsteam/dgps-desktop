cb2987b8db8aa36050868fc02982d4a95df64671
Proguranto <grtmega10@gmail.com>
2022-01-25 00:57:48 -0800

    Setup Maven Project

---
+++ src/main/java/me/huskyrobotics/App.java
@@ -0,0 +1,13 @@
+package me.huskyrobotics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
