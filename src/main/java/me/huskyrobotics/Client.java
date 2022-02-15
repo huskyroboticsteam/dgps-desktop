dfb852eaa02eacd05bc8ee149bd93cf79957f8f6
Grant Tannert <grtmega10@gmail.com>
2022-02-14 18:29:58 -0800

    Made Client get host name and port number from console input.

--- src/main/java/me/huskyrobotics/Client.java
+++ src/main/java/me/huskyrobotics/Client.java
@@ -7,12 +7,38 @@ import java.net.URISyntaxException;
 
 public class Client {
     public static void main(String[] args) throws URISyntaxException, InterruptedException {
-        WebSocketClient client = new WSClient(new URI("ws://localhost:3001"));
+        WWebSocketClient client = new WSClient(new URI("ws://" + getHost() + ":" + getPort()));
         client.connectBlocking();
         try {
-            client.send("hello");
+            // takes input from System.in and echoes message in server
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String userInput;
+            while (!(userInput = stdIn.readLine()).equals("STOP")) {
+                client.send(userInput);
+            }
+            client.close();
         } catch (Exception ex) {
             System.out.println("Client Failed to send message: " + ex.toString());
         }
+    }
+    
+    /**
+     * Gets user input for Host name.
+     * @return Host name.
+     */
+    private static String getHost() {
+        System.out.print("Host: ");
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
+    /**
+     * Gets user input for port number.
+     * @return Port Number.
+     */
+    private static int getPort() {
+        System.out.print("Port Number: ");
+        Scanner in = new Scanner(System.in);
+        return Integer.parseInt(in.nextLine());
     }
 }
